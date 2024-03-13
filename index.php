10c990951ff4ed154667a309d518d6bb01afaf36
BrianNixLaptop <briannormant@gmail.com>
2024-03-13 09:36:08 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap 5 Website Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+  <style>
+  .fakeimg {
+    height: 200px;
+    background: #aaa;
+  }
+  </style>
+</head>
+<body>
+
+<div class="p-5 bg-primary text-white text-center">
+  <h1>TCH099 Projet intégrateur en informatique - Gr. 05</h1>
+  <p>Projets de session</p> 
+</div>
+
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-sm-12">
+      <p>Page d'accueil du projet 6, test modification</p>
+    </div>
+  </div>
+</div>
+
+<div class="mt-5 p-4 bg-dark text-white text-center">
+<p>&copy; 2024 École de Technologie Supérieure, Montréal</p>
+</div>
+
+</body>
+</html>
