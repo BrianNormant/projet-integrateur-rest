e804cba0622ee0bf13fc338f25aa265b5c57c77a
Francis Corbin <jajsha@outlook.com>
2024-03-14 11:02:44 -0400

    Add landing skeleton

---
+++ jaime-les-trains/app/pages/LandingPage.tsx
@@ -0,0 +1,8 @@
+export function LandingPage() {
+    return (
+        <>
+            <h1>{"J'aime les trains"}</h1>
+            <h5>{"Votre solution pour votre gestion ferroviaire"}</h5>
+        </>
+    )
+}
