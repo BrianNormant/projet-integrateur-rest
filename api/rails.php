9bb384fbe16a785d39f899bb726995fe14e416c0
BrianNixDesktop <briannormant@gmail.com>
2024-03-19 10:59:54 -0400

    REST : Rails.php utilise la DB

--- api/rails.php
+++ api/rails.php
@@ -3,38 +3,18 @@
 header('Content-Type: application/json; charset-utf-8');
 http_response_code(400);
 
-function format($con1, $con2) {
-	return array(
-		"con1" => $con1,
-		"con2" => $con2
-	);
-};
-
-/*
 function formatRow($row) {
 	return array(
-		"con1" => $row->connection1,
-		"con2" => $row->connection2
+		"con1" => $row[0],
+		"con2" => $row[1]
 	);
 }
 include './api/connectDB.php';
 
-$sth = $dbh->query('SELECT connection1, connection2 FROM Rail06');
+$sth = $dbh->query('SELECT conn1_station, conn2_station FROM EQ06_Rail');
 $rails = array_map("formatRow", $sth->fetchAll());
-*/
 
-// ref: https://www.viarail.ca/sites/all/files/media/destinations/images/img-carte-canada-all-fr.svg
-$placeholder = array(
-	format("Toronto",     "Brockville"),
-	format("Brockville",  "Ottawa"),
-	format("Brockville",  "Montreal"),
-	format("Montreal",    "SainteFoy"),
-	format("SainteFoy",   "Quebec"),
-	format("Toronto",     "Winnipeg"),
-	format("Winnipeg",    "Edmonton"),
-	format("Edmonton",    "Vancouver"),
-);
 
-echo json_encode($placeholder);
+echo json_encode($rails);
 
 ?>
