a7d4c19c1c05e5080259432b6cefe412cbce0c38
BrianNixDesktop <briannormant@gmail.com>
2024-03-19 11:29:43 -0400

    RST: Stations.php utilise desormais la DB

--- api/stations.php
+++ api/stations.php
@@ -1,40 +1,28 @@
 <?php
-function format($name, $pos_x , $pos_y) {
-	return array(
-		"name" => $name,
-		"pos_x" => $pos_x,
-		"pos_y" => $pos_y
-	);
-};
 header('Content-Type: application/json; charset-utf-8');
 http_response_code(400);
 
-/*
-
 include './api/connectDB.php';
 
-$sth = $dbh->query('SELECT name, position_x, position_y FROM Station06');
-
-$stations = array();
-
-while($row = $sth->fetch()) {
-	$stations[] = format(
-		$row->name,
-		$row->position_x,
-		$row->position_y
+function format($row) {
+	return array(
+		"id" => $row["id"],
+		"name" => $row["nameStation"],
+		"pos_x" => $row["posX"],
+		"pos_y" => $row["posY"]
 	);
 }
-*/
 
+$sth = $dbh->query('SELECT id, nameStation, posX, posY FROM EQ06_Station');
+$stations = array_map("format", $sth->fetchAll());
 /*
  * https://www.viarail.ca/sites/all/files/media/destinations/images/img-carte-canada-all-fr.svg
  *
  * Prise des coordonées centré en haut a gauche.
  * Dimension de la carte: 1232 x 545
- *
+ * Dimension de la mise a l'echelle: 5972 * 2025
+ * 
  * Prise de dimension avec inkspace. Tranformation avec ./convertdim.c
- * */
-$placeholder = array(
 	format("Toronto",     1798,  1549),  #Toronto     861  417
 	format("Brockville",  1459,  1352),  #Brockville  931  364
 	format("Ottawa",      1575,  1293),  #Ottawa      907  348
@@ -44,7 +32,7 @@ $placeholder = array(
 	format("Winnipeg",    3466,  1237),  #Winnipeg    517  333
 	format("Edmonton",    4460,  855),   #Edmonton    312  230
 	format("Vancouver",   5289,  1078)   #Vancouver   141  290
-);
+ */
 
-echo json_encode($placeholder);
+echo json_encode($stations);
 ?>
