5d1b8ccb0c0713b754c21611d29a6336d547a717
BrianNixDesktop <briannormant@gmail.com>
2024-03-19 11:49:24 -0400

    REST; users.php utilise maitenant la DB

--- api/users.php
+++ api/users.php
@@ -3,15 +3,6 @@
 header('Content-Type: application/json; charset-utf-8');
 http_response_code(400);
 
-function format($user_name, $mail, $company) {
-	return [
-		"user_name" => $user_name,
-		"mail"      => $mail,
-		"company"   => $company
-	];
-}
-
-/*
 function formatRow($row) {
 	return array(
 		"user_name" => $row->user_name,
@@ -22,7 +13,6 @@ function formatRow($row) {
 include '/api/connectDB.php';
 $sth = $dbh->query('SELECT user_name,mail,company FROM User06');
 $users = array_map("formatRow", $sth->fetchAll());
-*/
 
 $placeholder = array(
 		format("brian",    "briannormant@bullshit.com",  "J'Aime-les-trains"),
