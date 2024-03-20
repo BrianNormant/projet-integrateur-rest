653f6ba52f7627bab605f918a17a47feebd270db
BrianNixDesktop <briannormant@gmail.com>
2024-03-20 16:20:46 -0400

    REST: fix error in check_token

--- api/check_token.php
+++ api/check_token.php
@@ -7,12 +7,23 @@
  * Return 2 if expired
  */
 function check_token($dbh, $token, $user) : int {
-	$sth = $dbh->prepare(<<<END
-	SELECT T.token AS tok, T.dateExpiration AS date FROM EQ06_Token T
-	INNER JOIN EQ06_Account A ON T.userUsed = A.userName
-	WHERE T.token = ?;
-	END);
-	$db_token = $sth->execute([$token])->fetchAll();
+	if (is_null($user)) {
+		$sth = $dbh->prepare(<<<SQL
+		SELECT T.token AS tok, T.dateExpiration AS date FROM EQ06_Token T
+		INNER JOIN EQ06_Account A ON T.userUsed = A.userName
+		WHERE T.token = ?;
+		SQL);
+		$sth->execute([$token]);
+		$db_token = $sth->fetchAll();
+	} else {
+		$sth = $dbh->prepare(<<<SQL
+		SELECT T.token AS tok, T.dateExpiration AS date FROM EQ06_Token T
+		INNER JOIN EQ06_Account A ON T.userUsed = A.userName
+		WHERE T.token = ? AND A.userName = ?;
+		SQL);
+		$sth->execute([$token, $user]);
+		$db_token = $sth->fetchAll();
+	}
 	if (sizeof($db_token) == 0) return 1;
 	if (strtotime($db_token[0]["date"]) < time()) return 2;
 	return 0;
