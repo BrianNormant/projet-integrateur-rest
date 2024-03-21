5f1ef9ca053b5add94f53973a79541c2a796d4f5
BrianNixDesktop <briannormant@gmail.com>
2024-03-21 00:53:30 -0400

    REST: refactor de reservation.php pour mettre le code en commun avec les autres endpoints

---
+++ api/lib_reservation.php
@@ -0,0 +1,81 @@
+<?php
+include './api/dijkstra.php';
+
+function stations_exist($dbh, $station_id) : bool {
+	$sth = $dbh->prepare(<<<SQL
+	SELECT id FROM EQ06_Station WHERE id = ?;
+	SQL);
+	$sth->execute([$station_id]);
+
+	return $sth->rowCount() > 0;
+}
+
+function get_rails($dbh, $origin, $destination) {
+	# Find path
+	$network = fetch_network_as_graph($dbh);
+	$path = dijkstra($network, $origin, $destination);
+
+	# Convert path to list of rails
+	$rails = array();
+	for ($i = 0; $i < sizeof($path) - 1; $i++)
+		$rails[] = array($path[$i], $path[$i+1]);
+	# Get Id of rails from DB
+	$sth_rail = $dbh->prepare(<<<SQL
+	SELECT id,conn1_station,conn2_station FROM EQ06_Rail WHERE conn1_station = ? AND conn2_station = ? OR conn1_station = ? AND conn2_station = ?;
+	SQL);
+	function rail_from_db($sth, $rail) {
+		$sth->execute([$rail[0], $rail[1], $rail[1], $rail[0]]);
+		$data = $sth->fetchAll()[0];
+		return array(
+			"id" => $data["id"],
+			"con1" => $data["conn1_station"],
+			"con2" => $data["conn2_station"],
+		);
+	}
+	return array_map(fn($rail) => rail_from_db($sth_rail, $rail), $rails);
+}
+
+function is_reservation_free($dbh, $rails, $possibility) : bool {
+	$sth = $dbh->prepare(<<<SQL
+	SELECT * FROM EQ06_Reservation RR
+	INNER JOIN EQ06_Rail R ON RR.rail_id = R.id
+	WHERE R.id = ? AND RR.dateReserv = ? AND RR.timeSlot = ?;
+	SQL);
+	foreach($rails as $rail) {
+		$sth->execute([$rail["id"], $possibility["date"], $possibility["period"]]);
+		if ($sth->rowCount() != 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function create_reservation($date, $period) {
+	$fare = 2500;
+	$tax_rate = 2;
+	$day = date_create($date)->getTimestamp() - date_create(date("Y-m-d"))->getTimestamp();
+	$day = round($day / (60*60*24));
+	for ($i = 0; $i < $day; $i++) $tax_rate = pow($tax_rate, 0.5);
+	$fare *= $tax_rate;
+	$fare = round($fare);
+
+	return array(
+		"date" => $date,
+		"period" => $period,
+		"fare" => $fare,
+	);
+}
+
+function gen_possibility($start) {
+	$shifts = array( 'morning', 'evening', 'night' );
+	$i = 0;
+
+	while (true) {
+		yield create_reservation($start->format("Y-m-d"), $shifts[$i++]);
+		if ($i >= sizeof($shifts)) {
+			$i = 0;
+			$start = date_add($start, date_interval_create_from_date_string("1 day"));
+		}
+	}
+}
+?>
--- api/reservation.php
+++ api/reservation.php
@@ -18,78 +18,25 @@ if ($origin == $destination) {
 }
 
 include './api/connectDB.php';
-include './api/dijkstra.php';
+include './api/lib_reservation.php';
 
-## Check if stations exists
-
-$sth = $dbh->prepare(<<<SQL
-SELECT id FROM EQ06_Station WHERE id = ? OR id = ?;
-SQL);
-$sth->execute([$origin, $destination]);
-
-$stations = $sth->fetchAll();
-if (sizeof($stations) != 2) {
+if (   !stations_exist($dbh, $origin) 
+	|| !stations_exist($dbh, $destination) ) {
 	http_response_code(404);
 	exit;
 }
 
-# Find path
-$network = fetch_network_as_graph($dbh);
-$path = dijkstra($network, $origin, $destination);
+$rails = get_rails($dbh, $origin, $destination);
 
-# Convert path to list of rails
-$rails = array();
-for ($i = 0; $i < sizeof($path) - 1; $i++)
-	$rails[] = array($path[$i], $path[$i+1]);
-# Get Id of rails from DB
-$sth_rail = $dbh->prepare(<<<SQL
-SELECT id,conn1_station,conn2_station FROM EQ06_Rail WHERE conn1_station = ? AND conn2_station = ? OR conn1_station = ? AND conn2_station = ?;
-SQL);
-function rail_from_db($sth, $rail) {
-	$sth->execute([$rail[0], $rail[1], $rail[1], $rail[0]]);
-	$data = $sth->fetchAll()[0];
-	return array(
-		"id" => $data["id"],
-		"con1" => $data["conn1_station"],
-		"con2" => $data["conn2_station"],
-	);
-}
-$rails = array_map(fn($rail) => rail_from_db($sth_rail, $rail), $rails);
-
-$start = date_create_from_format("Y-m-d H:i:s", $dbh->query("SELECT NOW();")->fetch()['NOW()']);
-$start = date_add($start, date_interval_create_from_date_string("1 day"));
-
-$reservations = $dbh->query("SELECT * FROM EQ06_Reservation;")->fetchAll();
 
 # Generate possible reservations excluding already reserved periods
-$sth = $dbh->prepare(<<<SQL
-SELECT * FROM EQ06_Reservation RR
-INNER JOIN EQ06_Rail R ON RR.rail_id = R.id
-WHERE R.id = ? AND RR.dateReserv = ? AND RR.timeSlot = ?;
-SQL);
-$shifts = array( 'morning', 'evening', 'night' );
-$i = 0;
-$tax_rate = 2;
+$start = date_create_from_format("Y-m-d H:i:s", $dbh->query("SELECT NOW();")->fetch()['NOW()']);
+$start = date_add($start, date_interval_create_from_date_string("1 day"));
 $possibilities = array();
-while (sizeof($possibilities) < 10) {
-	$possibility = array(
-		"date"   => date_format($start, "Y-m-d"),
-		"period" => $shifts[$i++],
-		"fare"   => round(2500 * $tax_rate),
-	);
-	if ($i >= sizeof($shifts)) {
-		$i = 0;
-		$start = date_add($start, date_interval_create_from_date_string("1 day"));
-		$tax_rate = pow($tax_rate, 0.5);
-	}
-	foreach($rails as $rail) {
-		$sth->execute([$rail["id"], $possibility["date"], $possibility["period"]]);
-		if ($sth->rowCount() != 0) {
-			goto next;
-		}
-	}
-	$possibilities[] = $possibility;
-	next:
+foreach(gen_possibility($start) as $possibility) {
+	if (is_reservation_free($dbh, $rails, $possibility))
+		$possibilities[] = $possibility;
+	if (sizeof($possibilities) >= 10) break;
 }
 
 echo json_encode(array(
