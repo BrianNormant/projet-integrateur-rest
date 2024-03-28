862dc17faa9be290876c5826a654035eadeaca00
BrianNixDesktop <briannormant@gmail.com>
2024-03-27 22:13:40 -0400

    commit somme test files

--- api/test/global.sh
+++ api/test/global.sh
@@ -5,8 +5,10 @@ USER='Etienne'
 PASSWD='1234'
 DATE=`date +%Y-%m-%d` # --date '5 day'`
 PERIOD='evening'
-ORIGIN=205 # Belleville
-DESTINATION=196 # Windsor
+# ORIGIN=205 # Belleville
+# DESTINATION=196 # Windsor
+ORIGIN=228 # Sudbury
+DESTINATION=229 # White River
 
 # login
 TOKEN=`curl -X PUT $URL/login/$USER -H "Authorization: $PASSWD" | sed -E 's/\{"token":"(.*)"\}/\1/'`
@@ -24,4 +26,7 @@ echo "Create reservation"
 curl -i -X PUT "$URL/reservations/$ORIGIN/$DESTINATION?date=$DATE&period=$PERIOD" -H "$HA"
 
 echo "Put train"
-curl -i -X PUT $URL/train/$ORIGIN/$DESTINATION -H "$HA" -d '{"charge":200,"puissance":4500}'
+# curl -i -X PUT $URL/train/$ORIGIN/$DESTINATION -H "$HA" -d '{"charge":200,"puissance":4500}'
+
+echo "List arrival at station"
+curl -i -X GET $URL/stations/198/arrivals -H "$HA"
--- api/test/train_for_station.php
+++ api/test/train_for_station.php
@@ -3,12 +3,12 @@
 include './api/connectDB.php';
 
 $sth = $dbh->query(<<<SQL
-SELECT * FROM EQ06_Train T
+SELECT T.id FROM EQ06_Train T
 INNER JOIN EQ06_RailRoute RR ON T.route_id = RR.route_id
-WHERE T.stop < RR.nb_stop
-;
-
-
+INNER JOIN EQ06_Rail R ON R.id = RR.rail_id
+INNER JOIN EQ06_Station S ON S.id IN (R.conn1_station, R.conn2_station)
+WHERE T.stop < RR.nb_stop AND T.lastStation NOT IN (R.conn1_station, R.conn2_station) AND S.id = 198
+GROUP BY T.id;
 SQL);
 
 var_dump($sth->fetchAll(PDO::FETCH_ASSOC));
